9398f614f53ec8ca2bda0cd1e858d88e174d6872
victor mbaye <victor.mbaye14@gmail.com>
2023-06-14 17:40:56 +0200

    added basic architecture, started implementing ESMFold 3Dstructure

--- app.py
+++ app.py
@@ -6,24 +6,12 @@ import py3Dmol
 import biotite.structure.io as bsio
 
 
-
-
-######################## FUNCTIONS ##########################
-
-# Render 3D molecule
-def render_mol(pdb):
-    pdbview = py3Dmol.view()
-    pdbview.addModel(pdb,'pdb')
-    pdbview.setStyle({'cartoon':{'color':'spectrum'}})
-    pdbview.setBackgroundColor('white')#('0xeeeeee')
-    pdbview.zoomTo()
-    pdbview.zoom(2, 800)
-    pdbview.spin(True)
-    showmol(pdbview, height = 500,width=800)
-
-
-
-
+# Page configuration
+st.set_page_config(
+    page_title="🏯 Pagodas", # => Quick reference - Streamlit
+    page_icon="🏯",
+    layout="centered", # wide
+    initial_sidebar_state="auto") # collapsed
 
 
 
@@ -32,9 +20,9 @@ def render_mol(pdb):
 ######################## LAYOUT ##########################
 
 
-'''
+st.markdown('''
 # Pagodas
-'''
+''')
 
 
 st.markdown('''
@@ -45,23 +33,7 @@ st.markdown('''
 # st.sidebar.title('Pagodas')
 st.write('[*ESMFold*](https://esmatlas.com/about) is an end-to-end single sequence protein structure predictor based on the ESM-2 language model. For more information, read the [research article](https://www.biorxiv.org/content/10.1101/2022.07.20.500902v2) and the [news article](https://www.nature.com/articles/d41586-022-03539-1) published in *Nature*.')
 
-# Protein sequence input
-# default seq Apical meristem formation protein-related
-DEFAULT_SEQ = """MEDDDAAYDLIKHELLYSEDEVIISRYLKGMVVNGDSWPDHFIEDANVFTKNPDKVFNSERPRFVIVKPRTEACGKTDGCDSGCWRIIGRDKLIKSEETGKILGFKKILKFCLKRKPIDYKRSWVMEEYRLTNNLNWKQDHVICKIRFMFEAEISFLLSKHFYTTSESVLENELLPSYGYYLSNTQEEDEFYLDAIMTSEGNEWPSYVTNNVYCLHPLELVDLQDRMFNDYGTCIFANKTCGETDKCDGGYWKILHGDKLIKSNFGKVIGFKKVFEFYETVRQIYLCDGEEVTVTWTIQEYRLSKNVKQNKVLCVIKLTYDR"""
-txt = st.text_area('Input sequence', DEFAULT_SEQ, height=275)
 
-
-
-
-import streamlit as st
-from stmol import showmol
-import py3Dmol
-import requests
-import biotite.structure.io as bsio
-
-#st.set_page_config(layout = 'wide')
-st.sidebar.title('🎈 ESMFold')
-st.sidebar.write('[*ESMFold*](https://esmatlas.com/about) is an end-to-end single sequence protein structure predictor based on the ESM-2 language model. For more information, read the [research article](https://www.biorxiv.org/content/10.1101/2022.07.20.500902v2) and the [news article](https://www.nature.com/articles/d41586-022-03539-1) published in *Nature*.')
 
 # stmol
 def render_mol(pdb):
@@ -75,8 +47,11 @@ def render_mol(pdb):
     showmol(pdbview, height = 500,width=800)
 
 # Protein sequence input
-DEFAULT_SEQ = "MGSSHHHHHHSSGLVPRGSHMRGPNPTAASLEASAGPFTVRSFTVSRPSGYGAGTVYYPTNAGGTVGAIAIVPGYTARQSSIKWWGPRLASHGFVVITIDTNSTLDQPSSRSSQQMAALRQVASLNGTSSSPIYGKVDTARMGVMGWSMGGGGSLISAANNPSLKAAAPQAPWDSSTNFSSVTVPTLIFACENDSIAPVNSSALPIYDSMSRNAKQFLEINGGSHSCANSGNSNQALIGKKGVAWMKRFMDNDTRYSTFACENPNSTRVSDFRTANCSLEDPAANKARKEAELAAATAEQ"
-txt = st.sidebar.text_area('Input sequence', DEFAULT_SEQ, height=275)
+# default seq Apical meristem formation protein-related
+st.info('🧬 Enter your protein sequence 🧬')
+
+DEFAULT_SEQ = "MEDDDAAYDLIKHELLYSEDEVIISRYLKGMVVNGDSWPDHFIEDANVFTKNPDKVFNSERPRFVIVKPRTEACGKTDGCDSGCWRIIGRDKLIKSEETGKILGFKKILKFCLKRKPIDYKRSWVMEEYRLTNNLNWKQDHVICKIRFMFEAEISFLLSKHFYTTSESVLENELLPSYGYYLSNTQEEDEFYLDAIMTSEGNEWPSYVTNNVYCLHPLELVDLQDRMFNDYGTCIFANKTCGETDKCDGGYWKILHGDKLIKSNFGKVIGFKKVFEFYETVRQIYLCDGEEVTVTWTIQEYRLSKNVKQNKVLCVIKLTYDR"
+txt = st.text_area('Input sequence', DEFAULT_SEQ, height=275)
 
 # ESMfold
 def update(sequence=txt):
@@ -84,33 +59,21 @@ def update(sequence=txt):
         'Content-Type': 'application/x-www-form-urlencoded',
     }
     response = requests.post('https://api.esmatlas.com/foldSequence/v1/pdb/', headers=headers, data=sequence)
-    name = sequence[:3] + sequence[-3:]
+
     pdb_string = response.content.decode('utf-8')
 
     with open('predicted.pdb', 'w') as f:
         f.write(pdb_string)
 
-    struct = bsio.load_structure('predicted.pdb', extra_fields=["b_factor"])
-    b_value = round(struct.b_factor.mean(), 4)
 
     # Display protein structure
     st.subheader('Visualization of predicted protein structure')
     render_mol(pdb_string)
 
-    # plDDT value is stored in the B-factor field
-    st.subheader('plDDT')
-    st.write('plDDT is a per-residue estimate of the confidence in prediction on a scale from 0-100.')
-    st.info(f'plDDT: {b_value}')
-
-    st.download_button(
-        label="Download PDB",
-        data=pdb_string,
-        file_name='predicted.pdb',
-        mime='text/plain',
-    )
-
-predict = st.sidebar.button('Predict', on_click=update)
 
 
-if not predict:
-    st.warning('🧬 Enter protein sequence data 🧬')
+predict = st.button('Predict', on_click=update)
+
+
+# if not predict:
+#     st.warning('🧬 Enter your protein sequence 🧬')
