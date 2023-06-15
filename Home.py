f76e90d4a29bbef45e24bbec0770a779c4dc9105
erika fallacara <erikafallacara@gmail.com>
2023-06-15 14:43:42 +0200

    update

--- Home.py
+++ Home.py
@@ -23,6 +23,24 @@ st.info('🧬 Enter your protein sequence 🧬')
 DEFAULT_SEQ = "MTMDKSELVQKAKLAEQAERYDDMAAAMKAVTEQGHELSNEERNLLSVAYKNVVGARRSSWRVISSIEQKTERNEKKQQMGKEYREKIEAELQDICNDVLELLDKYLILNATQAESKVFYLKMKGDYFRYLSEVASGENKQTTVSNSQQAYQEAFEISKKEMQPTHPIRLGLALNFSVFYYEILNSPEKACSLAKTAFDEAIAELDTLNEESYKDSTLIMQLLRDNLTLWTSENQGDEGDAGEGEN"
 txt = st.text_area('Input sequence', DEFAULT_SEQ, height=275)
 
+url='https://pagodas-74s6w3h4cq-od.a.run.app/predict?protein_sequence='+txt
+response = requests.get(url)
+dicto = response.json()
+
+df = pd.DataFrame(dicto).T
+df['weighted_probability'] = df['weighted_probability'].map(lambda x : round(x,2))
+df = df.drop(columns=['probability']).rename(columns={'function_name' : 'Function Name','weighted_probability' : 'Weighted Probability'})
+df['Function Name'] = df['Function Name'].map(lambda x :x.capitalize())
+
+base_url = 'https://www.ebi.ac.uk/QuickGO/services/ontology/go/terms/{ids}/chart?ids=GO%3A'
+apps = [base_url + go_id[3:] for go_id in list(df.index)]
+
+df['Graph'] = apps
+
+data_df = df[df['Weighted Probability'] > 0.05].sample(10)
+data_df.to_csv('go_terms.csv',index=False)
+
+
 button_predict = st.button(':violet[Predict]')
 
 if button_predict:
