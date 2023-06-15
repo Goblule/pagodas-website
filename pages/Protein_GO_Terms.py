40fa9f10ec80d3e968728226327b424d82978679
erika fallacara <erikafallacara@gmail.com>
2023-06-15 09:24:04 +0200

    api integrated

--- pages/Protein_GO_Terms.py
+++ pages/Protein_GO_Terms.py
@@ -8,10 +8,26 @@ import biotite.structure.io as bsio
 import pandas as pd
 from streamlit_extras.switch_page_button import switch_page
 
+
+
+
+
 go_id='3A1904659'
 
 def update_table():
+
     st.subheader('Predicted GO Terms')
+
+
+    txt = open("seq.txt", "r").read()
+
+    url='https://pagodas-74s6w3h4cq-od.a.run.app/predict?protein_sequence='+txt
+    response = requests.get(url)
+    dicto = response.json()
+    df = pd.DataFrame(dicto).T.head(4)
+    df['weighted_probability'] = df['weighted_probability'].map(lambda x : round(x,2))
+    df = df.drop(columns=['probability']).rename(columns={'function_name' : 'Function Name','weighted_probability' : 'Weighted Probability'})
+
 
     apps = [ 'https://www.ebi.ac.uk/QuickGO/services/ontology/go/terms/{ids}/chart?ids=GO%'+go_id,
             'https://www.ebi.ac.uk/QuickGO/services/ontology/go/terms/{ids}/chart?ids=GO%'+go_id,
@@ -19,11 +35,13 @@ def update_table():
             'https://www.ebi.ac.uk/QuickGO/services/ontology/go/terms/{ids}/chart?ids=GO%'+go_id
     ]
 
-    dicto = {'Name' : ['<name>','<name>','<name>','<name>'],
-            'Weighted Probability' : ['<weighted_probability>','<weighted_probability>','<weighted_probability>','<weighted_probability>'],
-            'Graph' : apps,}
+    # dicto = {'Name' : ['<name>','<name>','<name>','<name>'],
+    #         'Weighted Probability' : ['<weighted_probability>','<weighted_probability>','<weighted_probability>','<weighted_probability>'],
+    #         'Graph' : apps,}
 
-    data_df = pd.DataFrame(dicto,index=[f'GO:{go_id}',f'GO:{go_id}',f'GO:{go_id}',f'GO:{go_id}'])
+    # data_df = pd.DataFrame(dicto,index=[f'GO:{go_id}',f'GO:{go_id}',f'GO:{go_id}',f'GO:{go_id}'])
+    data_df = df
+    df['Graph'] = apps
 
     st.data_editor(
         data_df,
@@ -35,5 +53,6 @@ def update_table():
         hide_index=False,
         width=800
     )
+
 
 update_table()
