b34526c063638e29c3c0e29fd5347e8a2f1f2434
erika fallacara <erikafallacara@gmail.com>
2023-06-15 14:34:51 +0200

    update

--- pages/Protein_Structure.py
+++ pages/Protein_Structure.py
@@ -9,7 +9,27 @@ import pandas as pd
 from streamlit_extras.switch_page_button import switch_page
 
 txt = open("seq.txt", "r").read()
-print(txt)
+
+
+url='https://pagodas-74s6w3h4cq-od.a.run.app/predict?protein_sequence='+txt
+response = requests.get(url)
+dicto = response.json()
+
+df = pd.DataFrame(dicto).T
+df['weighted_probability'] = df['weighted_probability'].map(lambda x : round(x,2))
+df = df.drop(columns=['probability']).rename(columns={'function_name' : 'Function Name','weighted_probability' : 'Weighted Probability'})
+df['Function Name'] = df['Function Name'].map(lambda x :x.capitalize())
+
+base_url = 'https://www.ebi.ac.uk/QuickGO/services/ontology/go/terms/{ids}/chart?ids=GO%3A'
+apps = [base_url + go_id[3:] for go_id in list(df.index)]
+
+df['Graph'] = apps
+
+data_df = df[df['Weighted Probability'] > 0.05].sample(10)
+data_df.to_csv('go_terms.csv',index=False)
+
+
+
 
 # stmol
 def render_mol(pdb):
