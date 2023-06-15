fdb9d33b1b85145c85dbd23688a0a03d93f1547f
erika fallacara <erikafallacara@gmail.com>
2023-06-15 14:47:28 +0200

    update

--- pages/Protein_Structure.py
+++ pages/Protein_Structure.py
@@ -40,25 +40,25 @@ def update_pdb(sequence=txt):
 
 update_pdb(txt)
 
-# url='https://pagodas-74s6w3h4cq-od.a.run.app/predict?protein_sequence='+txt
-# response = requests.get(url)
-# dicto = response.json()
+url='https://pagodas-74s6w3h4cq-od.a.run.app/predict?protein_sequence='+txt
+response = requests.get(url)
+dicto = response.json()
 
-# df = pd.DataFrame(dicto).T
-# df['weighted_probability'] = df['weighted_probability'].map(lambda x : round(x,2))
-# df = df.drop(columns=['probability']).rename(columns={'function_name' : 'Function Name','weighted_probability' : 'Weighted Probability'})
-# df['Function Name'] = df['Function Name'].map(lambda x :x.capitalize())
+df = pd.DataFrame(dicto).T
+df['weighted_probability'] = df['weighted_probability'].map(lambda x : round(x,2))
+df = df.drop(columns=['probability']).rename(columns={'function_name' : 'Function Name','weighted_probability' : 'Weighted Probability'})
+df['Function Name'] = df['Function Name'].map(lambda x :x.capitalize())
 
-# base_url = 'https://www.ebi.ac.uk/QuickGO/services/ontology/go/terms/{ids}/chart?ids=GO%3A'
-# apps = [base_url + go_id[3:] for go_id in list(df.index)]
+base_url = 'https://www.ebi.ac.uk/QuickGO/services/ontology/go/terms/{ids}/chart?ids=GO%3A'
+apps = [base_url + go_id[3:] for go_id in list(df.index)]
 
-# df['Graph'] = apps
+df['Graph'] = apps
 
-# data_df = df[df['Weighted Probability'] > 0.05].sample(10)
-# data_df.to_csv('go_terms.csv',index=False)
+data_df = df[df['Weighted Probability'] > 0.05].sample(10)
+data_df.to_csv('go_terms.csv',index=False)
 
 
-# button_go_terms = st.button(':green[Predict Functionalities]')
+button_go_terms = st.button(':green[Predict Functionalities]')
 
 if button_go_terms:
     switch_page("Protein_Functionalities")
