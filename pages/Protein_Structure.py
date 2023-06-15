d9957adf384d3623decb0e909f512e5a81326211
erika fallacara <erikafallacara@gmail.com>
2023-06-15 12:40:22 +0200

    update

--- pages/Protein_Structure.py
+++ pages/Protein_Structure.py
@@ -41,7 +41,7 @@ def update_pdb(sequence=txt):
 
 update_pdb(txt)
 
-button_go_terms = t.button(':green[Predict Functionality]')
+button_go_terms = st.button(':green[Predict Functionality]')
 
 if button_go_terms:
     switch_page("Protein_GO_Terms")
